97c40387784c12745b5ebb3cbfbf21ccfde8874d
Rina Herlina <herlinarina27@gmail.com>
2021-12-16 22:23:28 +0700

    Menambahkan fungsi get, update, delete pada controller dan routes

--- controllers/brtController.js
+++ controllers/brtController.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const firebase = require('../db');
-const Angkot = require('../models/brt');
+const Brt = require('../models/brt');
 const firestore = firebase.firestore();
 
 const addBRT = async (req, res, next) => {
@@ -14,6 +14,77 @@ const addBRT = async (req, res, next) => {
     }
 }
 
+const getAllBRT = async (req, res, next) => {
+    try {
+        const brts = await firestore.collection('brt');
+        const data = await brts.get();
+        const brtArray = [];
+        if (data.empty) {
+            res.status(404).send('No BRT record found');
+        } else {
+            data.forEach(doc => {
+                const brt = new Brt (
+                    doc.id,
+                    doc.data().kodeBRT,                    
+                    doc.data().ruteMap,
+                    doc.data().biayaPelajar,
+                    doc.data().biayaUmum,
+                    doc.data().jamOperasi,
+                    doc.data().infoRute,
+                    doc.data().infoHalte,
+                    doc.data().halte,
+                    doc.data().namaHalte,
+                    doc.data().infoJamDatang
+                );
+                brtArray.push(brt)
+            });
+            res.send(brtArray);
+        }
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
+const getBRT = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const brt = await firestore.collection('brt').doc(id);
+        const data = await brt.get();
+        if (!data.exists) {
+            res.status(404).send('BRT with the given ID not found');
+        } else {
+            res.send(data.data());
+        }
+    } catch (error) {
+        res.status(400).send(error.message);
+    }    
+}
+
+const updateBRT = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const data = req.body;
+        const brt = await firestore.collection('brt').doc(id);
+        await brt.update(data);
+        res.send('BRT record updated successfuly');
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+const deleteBRT = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        await firestore.collection('brt').doc(id).delete();
+        res.send('Record deleted successfuly');
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
 module.exports = {
-    addBRT
+    addBRT,
+    getAllBRT,
+    getBRT,
+    updateBRT,
+    deleteBRT
 }
--- routes/brt-routes.js
+++ routes/brt-routes.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const {
-    addBRT
+    addBRT,
+    getAllBRT,
+    getBRT,
+    updateBRT,
+    deleteBRT
 } = require('../controllers/brtController');
 
 const router = express.Router();
 
 router.post('/brt', addBRT);
+router.get('/data-brt', getAllBRT);
+router.get('/data-brt/:id', getBRT);
+router.put('/data-brt/:id', updateBRT);
+router.delete('/data-brt/:id', deleteBRT);
+
 
 module.exports = {
     routes: router
