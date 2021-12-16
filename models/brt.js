f9968023348f413d77abce2ef2a01f02bf3298f7
Rina Herlina <herlinarina27@gmail.com>
2021-12-16 21:06:13 +0700

    Menambahkan brt.js pada models

---
+++ models/brt.js
@@ -0,0 +1,17 @@
+class Brt {
+    constructor(id, kodeBRT, ruteMap, biayaPelajar,
+        biayaUmum, jamOperasi, infoRute, infoHalte, infoJamDatang) {
+            this.id = id;
+            this.kodeBRT = kodeBRT;
+            this.deskripsi = deskripsi;
+            this.ruteMap = ruteMap;
+            this.biayaPelajar = biayaPelajar;
+            this.biayaUmum = biayaUmum;
+            this.jamOperasi = jamOperasi;
+            this.infoRute = infoRute;
+            this.infoHalte = infoHalte;
+            this.infoJamDatang = infoJamDatang
+        }
+}
+
+module.exports = Brt;
