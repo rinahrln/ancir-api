a78462be19443b40ed403bf668379910b76e0353
Rina Herlina <herlinarina27@gmail.com>
2021-12-16 21:12:18 +0700

    Menambahkan brtController.js pada folder controllers

---
+++ controllers/brtController.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const firebase = require('../db');
+const Angkot = require('../models/brt');
+const firestore = firebase.firestore();
+
+const addBRT = async (req, res, next) => {
+    try {
+        const data = req.body;
+        await firestore.collection('brt').doc().set(data);
+        res.send('Record saved successfuly');
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
